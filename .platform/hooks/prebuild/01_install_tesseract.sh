226c15448a81daf8a1a1f806357d01309f014ab8
rahul <xnotebook.in@gmail.com>
2026-01-01 23:30:02 +0530

    CFLF to LF

--- .platform/hooks/prebuild/01_install_tesseract.sh
+++ .platform/hooks/prebuild/01_install_tesseract.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# .platform/hooks/prebuild/01_install_tesseract.sh
 
-# 1. Install EPEL 9 repository (contains Tesseract for AL2023)
-sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+# Directly install Tesseract and English langpack
+sudo dnf install -y https://dl.fedoraproject.org/pub/fedora/linux/releases/39/Everything/x86_64/os/Packages/t/tesseract-5.3.3-2.fc39.x86_64.rpm \
+https://dl.fedoraproject.org/pub/fedora/linux/releases/39/Everything/x86_64/os/Packages/t/tesseract-langpack-eng-5.3.3-2.fc39.noarch.rpm --nogpgcheck
 
-# 2. Force install Tesseract and the English language pack
-sudo dnf install -y tesseract tesseract-devel tesseract-langpack-eng --nogpgcheck
-
-# 3. Ensure permissions are correct for the web user
+# Explicitly grant execution permissions to the binary for the web user
 sudo chmod +x /usr/bin/tesseract
